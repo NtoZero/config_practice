cc5835485df87d11c0ff8fafc01ff3699cdfbcc6
st.lee <st.lee@quintet.co.kr>
2025-06-24 14:53:24 +0900

    feat: p12 project init

---
+++ encrypt-file/src/main/java/demo/encryptfile/EncryptFileApplication.java
@@ -0,0 +1,13 @@
+package demo.encryptfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptFileApplication.class, args);
+    }
+
+}
