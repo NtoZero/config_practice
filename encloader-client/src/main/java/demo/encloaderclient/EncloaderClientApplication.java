25b79a9693cca4b2a442ecb3fb08ddd700ee744b
st.lee <st.lee@quintet.co.kr>
2025-06-26 08:58:57 +0900

    feat: enc_loader_client 추가

---
+++ encloader-client/src/main/java/demo/encloaderclient/EncloaderClientApplication.java
@@ -0,0 +1,13 @@
+package demo.encloaderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncloaderClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncloaderClientApplication.class, args);
+    }
+
+}
